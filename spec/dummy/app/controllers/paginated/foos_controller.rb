c3cd819332809cbf756a000f10ef00ec9180a4aa
klacointe <kevinlacointe@gmail.com>
2013-10-04 11:03:24 +0200

    [Dummy] Paginated controller use responders.

--- spec/dummy/app/controllers/paginated/foos_controller.rb
+++ spec/dummy/app/controllers/paginated/foos_controller.rb
@@ -1,6 +1,11 @@
 class Paginated::FoosController < ApplicationController
   extend Zobi
   behaviors :paginated
+  respond_to :html
+
+  def index
+    respond_with collection
+  end
 
   def paginate_per
     10
